7974c6d57b9d4565e07df751abd734e2578aa02a
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-27 13:33:56 +0100

    Initial Commit

---
+++ GeoAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoAlarm
+//
+//  Created by Pol Monne Parera on 27/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
