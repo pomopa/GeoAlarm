24fd7e5dc13fea8160032b7b5905e58ce367fb26
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-28 21:14:21 +0100

    Change password logic implemented

--- GeoAlarm/ProfileViewController.swift
+++ GeoAlarm/ProfileViewController.swift
@@ -15,6 +15,7 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadUserData()
     }
     
     override func viewDidLayoutSubviews() {
@@ -22,10 +23,64 @@ class ProfileViewController: UIViewController {
         profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
     }
     
+    private func loadUserData() {
+        guard let user = Auth.auth().currentUser else {
+            userEmailLabel.text = "Not logged in"
+            return
+        }
+
+        userEmailLabel.text = user.email
+    }
+    
     @IBAction func editPicture(_ sender: Any) {
     }
     
+    private func showSimpleAlert(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
+    private func updatePassword(currentPassword: String, newPassword: String) {
+        guard let user = Auth.auth().currentUser,
+              let email = user.email else { return }
+
+        let credential = EmailAuthProvider.credential(withEmail: email,
+                                                      password: currentPassword)
+
+        user.reauthenticate(with: credential) { _, error in
+            if let error = error {
+                self.showSimpleAlert("Error", error.localizedDescription)
+                return
+            }
+
+            user.updatePassword(to: newPassword) { error in
+                if let error = error {
+                    self.showSimpleAlert("Error", error.localizedDescription)
+                } else {
+                    self.showSimpleAlert("Success", "Password updated")
+                }
+            }
+        }
+    }
+
+    
     @IBAction func changePassword(_ sender: Any) {
+        let alert = UIAlertController(title: "Change Password",
+                                          message: nil,
+                                          preferredStyle: .alert)
+
+        alert.addTextField { $0.placeholder = "Current password"; $0.isSecureTextEntry = true }
+        alert.addTextField { $0.placeholder = "New password"; $0.isSecureTextEntry = true }
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Change", style: .default) { _ in
+            let current = alert.textFields?[0].text ?? ""
+            let new = alert.textFields?[1].text ?? ""
+            self.updatePassword(currentPassword: current, newPassword: new)
+        })
+
+        present(alert, animated: true)
     }
     
     @IBAction func deleteAccount(_ sender: Any) {
