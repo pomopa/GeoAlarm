6a60ebd97412ffd310750bc511204da6e4fbf47b
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-30 13:04:39 +0100

    General View Controllers implemented

---
+++ GeoAlarm/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  GeoAlarm
+//
+//  Created by Pol Monne Parera on 30/12/25.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
